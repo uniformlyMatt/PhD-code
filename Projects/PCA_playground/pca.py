3889478d2b516de32680e8e33e1874e8b20b7d9c
uniformlymatt <uniformlymatt@gmail.com>
2022-01-03 12:43:15 -0700

    Added PCA exercise script

---
+++ Projects/PCA_playground/pca.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy import ndarray
+
+
+def design_matrix(n_obs: int) -> np.ndarray:
+    """ Create the design matrix """
+
+    return np.random.binomial(1, p=.5, size=(n_obs, n_obs))
+
+
+def covariance(design: np.ndarray, biased=False):
+    """ Accepts design matrix and returns covariance matrix """
+    design = design.astype(np.float64)
+
+    mu = np.mean(design, axis=1)
+
+    factor = len(design[:, 0]) - 1
+
+    # center the design matrix
+    design -= mu[:, None]  # this is the same as mu.reshape(-1,1)
+
+    if biased:
+        factor += 1
+
+    return np.dot(design, design.T)*np.true_divide(1, factor)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    n = 5
+    x: ndarray = design_matrix(n)
+    cov = covariance(x)
